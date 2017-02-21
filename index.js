78527323c675ef5157a77ba032ebe3ef9c29bf42
sajeel93 <sajeel.ahmed786@gmail.com>
2017-02-21 11:57:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+/* GET home page. */
+router.get('/hello', function(req, res, next) {
+  res.render('hello', { title: 'Hello World' });
+});
+
+module.exports = router;
